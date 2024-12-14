b13edc45ab35572a7007c8ae2b76717e69edc381
g-gundam <gg@nowhere>
2024-12-14 00:48:47 -0800

    Add docstring for Chart struct

--- src/TechnicalIndicatorCharts.jl
+++ src/TechnicalIndicatorCharts.jl
@@ -24,6 +24,55 @@ using DocStringExtensions
     v::Float64
 end
 
+"""
+# Summary
+
+A Chart is a mutable struct that has:
+
+- a name (typically of the asset like "BTCUSD" or "AAPL")
+- a timeframe (which controls how much time each candle on the chart represents)
+- a DataFrame to hold OHLCV values and indicator values
+- a Vector of `OnlineTechnicalIndicator`s to display on the chart.
+- another Vector of display configuration for each indicator.
+
+If you were to go to [TradingView](https://tradingview.com/) and look at a chart, 
+imagine what kind of data structure would be required to represent it in memory.
+That's what the `Chart` struct aims to be.
+
+# Fields
+$(TYPEDFIELDS)
+
+# Constructors
+```julia
+Chart(name, tf; indicators=[], visuals=[])
+```
+
+# Examples
+
+```julia-repl
+julia> just_candles = Chart("ETHUSD", Minute(1))
+
+julia> golden_cross = Chart(
+    "BTCUSD", Hour(4);
+    indicators = [
+        SMA{Float64}(;period=50),
+        SMA{Float64}(;period=200)
+    ],
+    visuals = [
+        Dict(
+            :label_name => "SMA 50",
+            :line_color => "#E072A4",
+            :line_width => 2
+        ),
+        Dict(
+            :label_name => "SMA 200",
+            :line_color => "#3D3B8E",
+            :line_width => 5
+        )
+    ]
+)
+```
+"""
 mutable struct Chart
     # This is the user-facing data.
     name::AbstractString                     # name
