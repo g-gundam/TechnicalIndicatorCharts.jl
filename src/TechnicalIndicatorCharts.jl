408cfecdab71e86ed690136317cdbc42d2a5e801
g-gundam <gg@nowhere>
2024-12-15 20:28:57 -0800

    Show an example of using `nothing` for defaults

--- src/TechnicalIndicatorCharts.jl
+++ src/TechnicalIndicatorCharts.jl
@@ -35,7 +35,7 @@ A Chart is a mutable struct that has:
 - a Vector of `OnlineTechnicalIndicator`s to display on the chart.
 - another Vector of display configuration for each indicator.
 
-If you were to go to [TradingView](https://tradingview.com/) and look at a chart, 
+If you were to go to [TradingView](https://tradingview.com/) and look at a chart,
 imagine what kind of data structure would be required to represent it in memory.
 That's what the `Chart` struct aims to be.
 
@@ -53,24 +53,30 @@ Chart(name, tf; indicators=[], visuals=[])
 julia> just_candles = Chart("ETHUSD", Minute(1))
 
 julia> golden_cross = Chart(
-    "BTCUSD", Hour(4);
-    indicators = [
-        SMA{Float64}(;period=50),
-        SMA{Float64}(;period=200)
-    ],
-    visuals = [
-        Dict(
-            :label_name => "SMA 50",
-            :line_color => "#E072A4",
-            :line_width => 2
-        ),
-        Dict(
-            :label_name => "SMA 200",
-            :line_color => "#3D3B8E",
-            :line_width => 5
-        )
-    ]
-)
+           "BTCUSD", Hour(4);
+           indicators = [
+               SMA{Float64}(;period=50),
+               SMA{Float64}(;period=200)
+           ],
+           visuals = [
+               Dict(
+                   :label_name => "SMA 50",
+                   :line_color => "#E072A4",
+                   :line_width => 2
+               ),
+               Dict(
+                   :label_name => "SMA 200",
+                   :line_color => "#3D3B8E",
+                   :line_width => 5
+               )
+           ]
+       )
+
+julia> default_visuals = Chart(
+           "BNBUSDT", Hour(4);
+           indicators = [BB{Float64}(), StochRSI{Float64}()],
+           visuals    = [nothing,       nothing]  # To use defaults, pass in `nothing`.
+       )
 ```
 """
 mutable struct Chart
