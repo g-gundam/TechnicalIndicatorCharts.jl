7f6eb4fc98a5278235f71e1467d66202c72a084d
g-gundam <gg@nowhere>
2024-12-15 19:52:48 -0800

    Allow defaults to be specified with `nothing` for StochRSI

--- src/StochRSI.jl
+++ src/StochRSI.jl
@@ -10,22 +10,28 @@ end
 
 Visualize StochRSI using 2 lwc_lines.
 """
-function visualize(srsi::StochRSI, opts, df::DataFrame)
-    k_defaults = Dict(
+function visualize(srsi::StochRSI, opts::Union{AbstractDict,Nothing}, df::DataFrame)
+    k_kwargs = Dict(
         :label_name => "StochRSI K",
         :line_color => "#2962FF",
         :line_width => 1,
         :line_type  => LWC_CURVED
     )
-    k_kwargs = merge(k_defaults, opts[:k])
     #k_start = findfirst(!ismissing, df[!, :stochrsi_k])
-    d_defaults = Dict(
+    d_kwargs = Dict(
         :label_name => "StochRSI D",
         :line_color => "#FF6D00",
         :line_width => 1,
         :line_type  => LWC_CURVED,
     )
-    d_kwargs = merge(d_defaults, opts[:d])
+    if opts !== nothing
+        if haskey(opts, :k)
+            merge!(k_kwargs, opts[:k])
+        end
+        if haskey(opts, :d)
+            merge!(d_kwargs, opts[:d])
+        end
+    end
     #d_start = findfirst(!ismissing, df[!, :stochrsi_d])
     #@info "start" k_start d_start
     k = replace_missing_with(0, df[!, :stochrsi_k])
