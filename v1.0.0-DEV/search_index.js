10098fecb6984f2d52e8acc41c50bac887c88331
g-gundam <g-gundam@users.noreply.github.com>
2025-09-22 03:29:02 -0700

    Remove docs for nonexistent versions
    https://discourse.julialang.org/t/documenter-jl-removing-unwanted-versions-of-generated-documentation-in-2024/123690

--- v1.0.0-DEV/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TechnicalIndicatorCharts","category":"page"},{"location":"#TechnicalIndicatorCharts","page":"Home","title":"TechnicalIndicatorCharts","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TechnicalIndicatorCharts.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TechnicalIndicatorCharts]","category":"page"}]
-}
