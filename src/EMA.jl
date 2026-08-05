35e552d8d607dc27d477c1ba379002d9e9cf67ff
g-gundam <g-gundam@users.noreply.github.com>
2026-08-04 23:22:25 -0700

    Add a config method to EMA

--- src/EMA.jl
+++ src/EMA.jl
@@ -2,16 +2,25 @@ denominated_price(ema::EMA) = true
 
 """$(TYPEDSIGNATURES)
 
+Return the default visualization config for EMA.
+"""
+function config(ema::EMA)
+    [lwc_line,
+     Dict(
+         :label_name => "EMA $(ema.period)",
+         :line_color => "#058ED9",
+         :line_width => 2
+     )]
+end
+
+"""$(TYPEDSIGNATURES)
+
 Visualize EMA using 1 lwc_line.
 """
 function visualize(ema::EMA, opts::Union{AbstractDict,Nothing}, df::DataFrame)
     start = ema.period
     name = indicator_fields(ema)[1]
-    kwargs = Dict(
-        :label_name => "EMA $(ema.period)",
-        :line_color => "#058ED9",
-        :line_width => 2
-    )
+    (fn, kwargs) = config(ema)
     if opts !== nothing
         merge!(kwargs, opts)
     end
