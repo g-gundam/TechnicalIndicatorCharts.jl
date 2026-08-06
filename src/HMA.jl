a85034e2086430239e04a5cb7a9e2b07ede97cb6
g-gundam <g-gundam@users.noreply.github.com>
2026-08-05 23:52:24 -0700

    Add config method to HMA

--- src/HMA.jl
+++ src/HMA.jl
@@ -2,16 +2,25 @@ denominated_price(hma::HMA) = true
 
 """$(TYPEDSIGNATURES)
 
+Return default visualization config for HMA.
+"""
+function config(hma::HMA)
+    [lwc_line,
+     Dict(
+         :label_name => "HMA $(hma.period)",
+         :line_color => "#EAC435",
+         :line_width => 2
+     )]
+end
+
+"""$(TYPEDSIGNATURES)
+
 Visualize HMA using 1 lwc_line.
 """
 function visualize(hma::HMA, opts::Union{AbstractDict,Nothing}, df::DataFrame)
     start = hma.period
     name = indicator_fields(hma)[1]
-    kwargs = Dict(
-        :label_name => "HMA $(hma.period)",
-        :line_color => "#EAC435",
-        :line_width => 2
-    )
+    (fn, kwargs) = config(hma)
     if opts !== nothing
         merge!(kwargs, opts)
     end
