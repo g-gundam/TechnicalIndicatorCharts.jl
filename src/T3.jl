744fdf07623e4e9c661d1b6060bd28a4d3b60456
g-gundam <g-gundam@users.noreply.github.com>
2026-08-07 23:55:18 -0700

    Add config method to T3

--- src/T3.jl
+++ src/T3.jl
@@ -2,16 +2,25 @@ denominated_price(t3::T3) = true
 
 """$(TYPEDSIGNATURES)
 
+Return the default visualization config for T3.
+"""
+function config(t3::T3)
+    [lwc_line,
+     Dict(
+         :label_name => "T3 $(t3.period)",
+         :line_color => "#FF69B4",
+         :line_width => 2
+     )]
+end
+
+"""$(TYPEDSIGNATURES)
+
 Visualize T3 using 1 lwc_line.
 """
 function visualize(t3::T3, opts::Union{AbstractDict,Nothing}, df::DataFrame)
     start = t3.period
     name = indicator_fields(t3)[1]
-    kwargs = Dict(
-        :label_name => "T3 $(t3.period)",
-        :line_color => "#FF69B4",
-        :line_width => 2
-    )
+    (fn, kwargs) = config(t3)
     if opts !== nothing
         merge!(kwargs, opts)
     end
